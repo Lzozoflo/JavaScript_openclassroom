86056c8b84704803bad386818e78ccbc1aea7b79
Florent Cretin <florent.cretin@hotmail.fr>
2026-01-14 09:09:49 +0100

    start part1c4

--- Cours-Dynamique/Part_1_C_4/pieces.js
+++ Cours-Dynamique/Part_1_C_4/pieces.js
@@ -2,7 +2,7 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 // Création des balises 
-const article = pieces[0];
+const article = pieces[3];
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
 const nomElement = document.createElement("h2");
@@ -11,9 +11,28 @@ const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+
+
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+
+const disponibleElement = document.createElement("p");
+disponibleElement.innerText = (article.disponibilite ? "En stock" : "Rupture de stock");
+
+
+
+
+
 //Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibleElement);
+
+
+// un élément de paragraphe pour la description avec le texte “Pas de description pour le moment.” en cas d’absence de description ;
+
+// un élément de paragraphe avec le texte “En stock” si l’article est disponible, ou “Rupture de stock” s’il n’est plus disponible.
